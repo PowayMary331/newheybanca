0823c103f3e1214983bc4314f400f1334f70c5ef
PowayMary331 <129775772+PowayMary331@users.noreply.github.com>
2023-04-03 20:15:35 +0000

    eqw

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -17,11 +17,13 @@ const Layout = ({ props, children, disablePaddingBottom = false }) => {
         {"<!-- Google tag (gtag.js) -->"}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-KKMGVSMVGM"></script>
         <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-          gtag('config', 'G-KKMGVSMVGM');
+            gtag('config', 'G-KKMGVSMVGM');
+          `}
         </script>
         <link
           rel="stylesheet"
